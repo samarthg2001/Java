6b9ceb446b5f15aeae6b676d35c5c12f8cd08bac
samarth <samganamote@gmail.com>
2025-10-13 20:00:43 +0530

    Day 1

---
+++ Number/Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Fibonacci{
+
+    public static void main(String arhs[]){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number of Fibonacci series");
+        int num=s.nextInt();
+        int first=0,second=1;
+        int sum=0;
+        for(int i=0;i<=num-1;i++){
+             System.out.print(" "+first+" ");
+            sum=first+second;
+            first=second;
+            second=sum;
+
+            // System.out.print(" "+sum+" ");
+        }
+    }
+}
