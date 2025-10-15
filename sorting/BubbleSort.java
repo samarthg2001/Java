81c209ff9b59cb1ffc1ab2d800fea97fe9cb363b
samarth <samganamote@gmail.com>
2025-10-15 19:47:54 +0530

    day3

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class BubbleSort {
+    public static void main(String args[]) {
+        // Scanner s = new Scanner(System.in);
+        // int n = s.nextInt(); // (optional if you want user input)
+        int[] arr = {10, 20, 3, 4, 56, 9, 8};
+        int temp;
+        boolean swapped;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
