c437bc93b4d5ca8a6917b5a483b729f3c3a7134f
samarth <samganamote@gmail.com>
2025-11-09 19:56:42 +0530

    day-1

---
+++ string/count.java
@@ -0,0 +1,48 @@
+package string;
+import java.util.*;
+
+public class count {
+
+   static int countVowels(String s){
+        String vowels[]={"a","e","i","o","u"};
+        String compare=s.toLowerCase();
+        int vowel=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j< vowels.length;j++){
+                if(compare.charAt(i) == vowels[j].charAt(0)){
+                    vowel++;
+                }
+
+            }
+        }
+
+        return vowel;
+    }
+
+
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value");
+
+
+        String a=sc.next();
+ 
+
+        for(int i=0;i<a.length();i++){
+            System.out.println(a.charAt(i));
+        }
+
+        int vowelscount=countVowels(a);
+        
+        int n=a.length();
+
+        if(n==vowelscount)
+        System.out.println("the number vouwels count in "+a+ " is  "+vowelscount);
+        else
+        System.out.println("the number vouwels count in "+a+ " is  "+vowelscount+" constant count is "+(n-vowelscount) );
+
+
+
+
+    }
+}
