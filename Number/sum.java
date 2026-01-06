22b6e566e26d61ce17b9a98e3c0954af3e741c1b
samarth <samganamote@gmail.com>
2026-01-06 22:42:29 +0530

    day 2

--- Number/sum.java
+++ Number/sum.java
@@ -9,7 +9,27 @@ public class sum {
             num=num/10;
 
         }
-System.out.println(sum);
+        boolean value=armstrong(372);
+System.out.println(sum+ ""+value);
     }
 
+
+
+    static boolean armstrong( int num){
+    boolean flag=false;
+    int sum=0,value=0;
+    int digit=String.valueOf(num).length();
+        int a=num;
+    while(num>0){
+        value=num%10;
+        sum+=Math.pow(value,digit);
+        // sum=sum+(value*value*value);
+        num=num/10;
+        }    
+if(sum==a){
+    flag=true;
+}
+
+        return flag;        
+    }
 }
