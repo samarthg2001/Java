101fccc87c1fc2c5d244fb761f492c3c8ab1007f
samarth <samganamote@gmail.com>
2026-04-12 16:33:11 +0530

    day 0

--- arrayList/create.java
+++ arrayList/create.java
@@ -25,18 +25,84 @@ public class create {
             int i=0;
             int value=0;
             while(i<n){
-                System.out.print(value+" ");
+                System.out.print(a+" ");
                 value=a+b;
                 a=b;
                 b=value;
                 n--;
             }
-
-            
         }
+
+
+
+        public static void primeNumber(int n){
+            if(n<=1){
+                System.out.println(n+" is not a prime number");
+            }
+             for(int i=2;i<=n;i++){
+                if(n%i==0)
+     {System.out.println(n +" is  prime number");}
+
+
+
+            }
+
+        }
+
+
+        public static void secondHighest(int arr[]){
+            int len=arr.length;
+            int highest=0;
+            int secondHighest=0;
+            int thridhighest=0;
+            if(len<3){
+System.out.println("less then three number are there");
+            }
+
+            for(int i=0;i<=len-1;i++){
+
+                if(highest<arr[i]){
+                    thridhighest=secondHighest;
+                    secondHighest=highest;
+                    highest=arr[i];
+                }
+                else if(secondHighest<arr[i] ){
+                    thridhighest=secondHighest;
+                    secondHighest=arr[i];
+                }
+                else if (thridhighest<arr[i]) {
+                    thridhighest=arr[i];
+                }
+            }
+
+System.out.println(highest+" is highest"+secondHighest+" is secondhighest"+thridhighest+" is thirdhighest");
+
+        }
+
+        public static void findDuplicate(int arr[]){
+            for(int i=0;i<=arr.length-1;i++){
+                for(int j=i+1;i<=arr.length-1;i++){
+                    if(arr[i]==arr[j]){
+                        System.out.println(arr[j]+" is a duplicate value");
+                        break;
+                    }
+                }
+                break;
+
+            }
+        }
+
+
+
     public static void main(String [] args){
 ReverseString("MADA");
 fibonacciSeries(15);
-    }
+System.out.println();
+// primeNumber(15);
+    int [] arr={11,1,1,2,3,6,4,5,8,9,7,4,4,55,2,0,3,5,66,9,50};
+secondHighest(arr);
+findDuplicate(arr);
+
+}
     
 }
